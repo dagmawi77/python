05003533783f3352d5bae95a36dbb65eaf7dbb39
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-14 07:09:13 +0300

    illegal variable name

---
+++ illegal_variable.py
@@ -0,0 +1,10 @@
+Var Name = 1
+2ndvarname = 2
+Var-name = 3
+
+print(Var Name)
+print(2ndvarname)
+print(Var-name)
+# Output: SyntaxError: invalid syntax
+# Explanation: Variable names cannot have spaces, start with a number or have special characters.
+# This will raise a syntax error.
