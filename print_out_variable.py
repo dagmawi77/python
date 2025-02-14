34085943157338ee2ef215d42af666593c5ad720
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-14 07:30:01 +0300

    print out variable

---
+++ print_out_variable.py
@@ -0,0 +1,16 @@
+x = "print out variable"
+print(x)
+x = "Print"
+y=" Out"
+z=" Variable"
+print(x+y+z)
+print(x,y,z)    # Print Out Variable
+x=5
+y=10
+z=15
+print(x+y+z)    # 30
+
+x =5
+y= "Variable"
+#print(x+y)  # TypeError: unsupported operand type(s) for +: 'int' and 'str'
+print(x,y)  # 5 Variable
