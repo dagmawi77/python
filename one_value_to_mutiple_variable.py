246818330f14b07b31d74ae77ba191e66afd0944
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-14 07:20:11 +0300

    One Value to Mutiple Variable

---
+++ one_value_to_mutiple_variable.py
@@ -0,0 +1,9 @@
+x=y=z="The one Value"
+print(x)   # The one Value
+print(y)   # The one Value
+print(z)   # The one Value
+# Explanation:
+# In this example, we have assigned the same value to multiple variables in a single line.
+# The values can be of any data type.
+# The variables can be of any data type.
+# The variables can be defined in a single line.
