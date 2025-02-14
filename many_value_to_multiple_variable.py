739a5fcec7cae3994b0dcd616dcd669ddad0bc14
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-14 07:18:07 +0300

    many value to mutiple variable

---
+++ many_value_to_multiple_variable.py
@@ -0,0 +1,9 @@
+x,y,z="the x value","the y value","the z value"
+print(x)   # the x value
+print(y)   # the y value
+print(z)   # the z value
+# Explanation:
+# In this example, we have assigned multiple values to multiple variables in a single line.
+# The number of variables must match the number of values in the sequence.
+# The values must be separated by commas.
+# The values can be of any data type.
