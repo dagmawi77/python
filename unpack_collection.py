e0fc7469b271ac3c5fe1ba40e8b89468403d4bbf
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-14 07:24:19 +0300

    Unpack Collection

---
+++ unpack_collection.py
@@ -0,0 +1,9 @@
+fruit = ["orange", "apple", "banana"]
+x,y,z=fruit
+print(x)
+print(y)
+print(z)
+# Explanation:
+# In this example, we have unpacked a collection into multiple variables.
+# The number of variables must match the number of values in the collection.
+# The values are unpacked in order.
