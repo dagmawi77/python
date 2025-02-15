8c61b7042d6752b23d8106f0adcd6aff4c2b6107
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-15 20:05:51 +0300

    Global Function

---
+++ global_function.py
@@ -0,0 +1,5 @@
+x = "Awsome"
+def myfunc():
+    print("Python is " + x)
+myfunc()  # Python is Awsome
+# Explanation: The x variable is a global variable. It can be used by the functions inside the function.    
