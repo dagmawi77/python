6b4c3e1a850f82d142f2a665ee68850ad272d853
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-16 13:11:26 +0300

    Number and Data type convertion

---
+++ multi_word_variable_name.py
@@ -0,0 +1,7 @@
+myVariableName = "this is Camel Case"
+MyVariableName = "this is Pascal Case"
+my_variable_name = "this is Snake Case"
+print(myVariableName)  # Output: this is Pascal Case
+print(MyVariableName)  # Output: this is Pascal Case
+print(my_variable_name)  # Output: this is Snake Case
+# Explanation: Python does not have a specific naming convention for multi-word variable names.
