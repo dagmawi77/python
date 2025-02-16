ef78eaa2d3f16d6546beec95630ab509b19eb54d
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-16 13:12:13 +0300

    Number and Data type convertion

---
+++ number.py
@@ -0,0 +1,55 @@
+import random
+# = int or integer is a whole number, positive or negative, without decimals, of unlimited length. 
+x = 1
+y = 5345323546
+z = -3255522
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+# = float is a number, positive or negative, containing one or more decimals.
+x = 1.10
+y = 1.536453573537537435437
+z = -35.22
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+# e or E is used to represent 10 to the power of.
+x = 35e3
+y = 12E4
+z = -87.7e100
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+# = complex numbers are written with a "j" as the imaginary part.
+x = 3+5j
+y = 5j
+z = -5j
+print(x)
+print(y)
+print(z)
+print(type(x))
+print(type(y))
+print(type(z))
+# = Type Conversion
+a = float(x)
+b= int(y)
+c = complex(z)
+print(a)
+print(b)
+print(c)
+
+print(type(a))
+print(type(b))
+print(type(c))
+
+#random number
+print(random.randrange(1,10)) # Output: Random number between 1 and 10
--- numbers.py
+++
@@ -1,6 +0,0 @@
-x = 1
-y = 5345323546
-z = -3255522
-print(x)
-print(y)
-print(z)
