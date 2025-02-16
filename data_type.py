7ff12c7a83f4ca700c930dcf5aa42891e631768e
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-16 12:46:00 +0300

    data Type

---
+++ data_type.py
@@ -0,0 +1,69 @@
+a = "String"
+b = 10 # Integer
+c = 10.5 # Float
+d = True # Boolean
+e = 1J # Complex
+f= [1,2,3,4,5] # List
+g = (1,2,3,4,5) # Tuple
+h = {1,2,3,4,5} # Set
+i =({1,2,3,4,5}) #frozen set
+j = {"name":"John", "age":30} # Dictionary
+k = b"hello" # Bytes
+l = bytearray(5) # Byte Array
+m = memoryview(bytes(5)) # Memory View
+J= 10
+n={"apple", "banana", "cherry"}
+o=frozenset({"apple", "banana", "cherry"})
+p=None
+print(J)
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+print(j)
+print(k)
+print(l)
+print(m)
+print(n)
+print(o)
+print(p)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
+print(type(m))
+print(type(n))
+print(type(o))
+print(type(p))
+
+# Explanation: In Python, variables do not need explicit declaration to reserve memory space. The declaration happens automatically when you assign a value to a variable. The equal sign (=) is used to assign values to variables.
+# The operand to the left of the = operator is the name of the variable and the operand to the right of the = operator is the value stored in the variable.
+# Python has five standard data types −
+# Numbers
+# String
+# List
+# Tuple
+# Dictionary
+# Python supports four different numerical types −
+# data type types 
+# Text Type:	str
+# Numeric Types:	int, float, complex
+# Sequence Types:	list, tuple, range
+# Mapping Type:	dict
+# Set Types:	set, frozenset
+# Boolean Type:	bool
+# Binary Types:	bytes, bytearray, memoryview
+# None Type:	NoneType
