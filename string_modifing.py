9dad79c60e90ca8c9a8c059a69835a3dd8179e45
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-17 07:24:52 +0300

    String Modification

---
+++ string_modifing.py
@@ -0,0 +1,11 @@
+a = "Hello, World!"
+# Modify Strings to uppercase
+print(a.upper())
+# Modify Strings to lowercase
+print(a.lower())
+# Remove Whitespace
+print(a.strip())  # returns "Hello, World!"
+# Replace String
+print(a.replace("H", "J"))
+# Split String
+print(a.split(","))  # returns ['Hello', ' World!']
