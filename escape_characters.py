2690d43ecd12a336f83b7edcbff6084341429bd2
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-18 07:08:11 +0300

    Escape Characters in Python

---
+++ escape_characters.py
@@ -0,0 +1,23 @@
+# escape characters the backslash "\" is used to escape characters that otherwise have a special meaning, such as newline, backslash, or the quote character.
+# escape characters in python are used to remove the special meaning from a character.
+txt = "Escape characters in python \"Escape\" the character"
+print(txt)
+# new line
+txt = "Escape characters in python \nEscape the character"
+print(txt)
+
+txt = "Escape characters in python \tEscape the character"
+print(txt)
+
+txt = "Escape characters in python \bEscape the character"
+print(txt)
+txt = "Escape characters in python \rEscape the character"
+print(txt)
+txt = "Escape characters in python \fEscape the character"
+print(txt)
+txt = "Escape characters in python \vEscape the character"
+print(txt)
+txt = "Escape characters in python \aEscape the character"
+print(txt)
+txt = "Escape characters in python \\ Escape the character"
+print(txt)
