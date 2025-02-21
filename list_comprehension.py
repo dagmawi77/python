4233d1198d913d714c1cb6f6aca4c9833759c2b9
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-21 21:11:32 +0300

    list Comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,9 @@
+vegitable = ['potato', 'tomato', 'onion', 'carrot', 'cucumber']
+ovegitable=[]
+
+for x in vegitable:
+    if "o" in x:
+        ovegitable.append(x)
+
+print(vegitable)
+print(ovegitable)
