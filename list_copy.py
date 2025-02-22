f3acfc9b2a320877b9a49ab363fafdd25467d96a
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-22 20:51:40 +0300

    Copy list

---
+++ list_copy.py
@@ -0,0 +1,11 @@
+mylist = ["abebe", "kebede", "bekele","lema"]
+
+newnamelist = mylist.copy()
+
+print(newnamelist)
+
+newnamelistnew = list(mylist)
+print(newnamelistnew)
+
+newlist3 = mylist[:]
+print(newlist3)
