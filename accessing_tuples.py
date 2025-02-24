9ea34c04a6ed03959657260d710b9f6cf6711ffb
Dagmawi Letarik <dagmawiletarik@gmail.com>
2025-02-24 07:09:46 +0300

    Accesing tuples

--- accessing_tuples.py
+++ accessing_tuples.py
@@ -3,7 +3,7 @@ thisistuple=('a','b','c','d','e','f')
 print(thisistuple[0]) #get the first iteam in tuples 
 print(thisistuple[-1]) #get the last iteams 
 print(thisistuple[2:5]) # get range between 2 5 but it is not include 5 
-print(thisistuple[2:0]) # get range starting from 2
+print(thisistuple[2:]) # get range starting from 2
 print(thisistuple[:4]) # get range before 4 and not include 4
 print(thisistuple[-4:-1])
 
