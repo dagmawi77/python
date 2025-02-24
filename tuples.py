e5aa08d9bd3584af43ef614cbb4659a85fbf09ef
Dagmawi Letarik <dagmawiletarik@gmail.com>
2025-02-24 06:53:50 +0300

    Tuple declaration

--- tuples.py
+++ tuples.py
@@ -1,3 +1,10 @@
 #exaple for tuples in python
 thisistuple=('abebe','kebede','lema')
 print(thisistuple)
+print(len(thisistuple))
+print(type(thisistuple))
+# constructor of tuple 
+
+thisistuple=(('abebe','kebede','lema'))
+
+print(thisistuple)
