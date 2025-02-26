77b1eeba2bad7bf70c58496a62460c9aa7b9d8a4
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-26 07:33:19 +0300

    Tuple List

---
+++ tuple_list.py
@@ -0,0 +1,12 @@
+fruite = ("banan","oranage","Mango")
+for x in fruite:
+    print(x)
+
+for i in range(len(fruite)):
+    print(fruite[i])
+
+b=0
+
+while b< len(fruite):
+    print(fruite[b])
+    b = b +1
