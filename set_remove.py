ac826640f6c5f3ba6f38e505cee4bcc1a289d753
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-27 07:33:02 +0300

    Removing Iteam from list

---
+++ set_remove.py
@@ -0,0 +1,13 @@
+thisisset={"set1","set2","set3","set4",7,8,9,10}
+thisisset.remove("set1")
+print(thisisset)
+thisisset.discard(8)
+x= thisisset.pop()
+print(x)
+
+thisisset.clear()
+print(thisisset)
+
+#del thisisset
+
+print(thisisset)
