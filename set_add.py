e7afd6435c61cf6dc5d2a053542c412b056b60c5
dagmawi77 <dagmawiletarik@gmail.com>
2025-02-27 07:26:39 +0300

    Set add

---
+++ set_add.py
@@ -0,0 +1,7 @@
+thisissetone={"set1","set2"}
+thisissetone.add("set3")
+print(thisissetone)
+thesecondset={"set4","set5",6,7}
+thisissetone.update(thesecondset)
+print(thesecondset)
+print(thisissetone)
