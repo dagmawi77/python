9b0e0e13c8dd5d99678a9786c464276e645d33f7
dagmawi77 <dagmawiletarik@gmail.com>
2025-03-03 07:21:24 +0300

    Dictionaires Copy

---
+++ dictionaires_copy.py
@@ -0,0 +1,12 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict.items()
+
+mydict = thisdict.copy()
+mydict2 = dict(thisdict)
+
+print(mydict)
+print(mydict2)
