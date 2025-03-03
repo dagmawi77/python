9c75854e79409600fb6354653261162f73b53d18
dagmawi77 <dagmawiletarik@gmail.com>
2025-03-03 06:56:37 +0300

    git access and change

---
+++ dictionary_access.py
@@ -0,0 +1,39 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict["model"]
+print(x)
+x = thisdict.get("year")
+print(x)
+x= thisdict.keys()
+print(x)
+
+thisdict["color"]="white"
+print(x)
+
+x = thisdict.values()
+
+print(x)
+
+thisdict["year"]=2000
+
+print(x)
+
+thisdict["color"]="red"
+print(x)
+
+x = thisdict.items()
+
+print(x)
+
+thisdict["year"]=20005
+print(x)
+
+if "model" in thisdict:
+    print("Yes The model is present in Dectionary")
+
+thisdict.update({"year":2005})
+
+print(x)
