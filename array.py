3d621f9fab8eae718a15224deae00af0d54e99b8
dagmawi77 <dagmawiletarik@gmail.com>
2025-03-06 07:02:14 +0300

    array example in list

---
+++ array.py
@@ -0,0 +1,12 @@
+carname = ["Toyota","Ford","Volvo","Mitsbush"]
+x = carname[0]
+print(x)
+
+carname.append("Tesla")
+print(carname)
+
+print(len(carname))
+carname.pop(1)
+print(carname)
+carname.remove("Volvo")
+print(carname)
