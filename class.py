63b348b2e88404da2ad38c36fd2f007c4a97b5b3
dagmawi77 <dagmawiletarik@gmail.com>
2025-03-07 06:49:06 +0300

    Classs and object

---
+++ class.py
@@ -0,0 +1,26 @@
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+p1 = Person("dagmawi",29)
+
+print(f"his name is {p1.name} and his age is {p1.age}")
+
+#Create Class 
+class Person1:
+  def __init__(self,name,age):
+    self.name=name
+    self.age=age
+  def __str__(self):
+     return f"His name is {self.name} and His age is {self.age}"
+  def myfunc(self):
+      print("Hello my name is " + self.name)
+
+
+oneperson = Person1("dagmawi",30)
+oneperson.myfunc()
+
+print(oneperson)
+
+  
