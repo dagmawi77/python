c0172f0a04295fabea64fee111bf81b033ec5398
Dagmawi Letarik <dagmawiletarik@gmail.com>
2025-03-10 16:37:00 +0300

    challenge 5 comment

---
+++ challenge_ 5_comment.py
@@ -0,0 +1,10 @@
+"""Write a Python program that contains a commented line and then outputs 'Cheers, Mate!' to the console.
+
+Requirements
+
+The program should contain at least one comment.
+
+The output should be 'Cheers, Mate!'."""
+
+#print("Hello, World!")
+print("Cheers, Mate!")
