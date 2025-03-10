5d6d0615a07459036cc5f9630b6e78a4bec42d31
Dagmawi Letarik <dagmawiletarik@gmail.com>
2025-03-10 10:21:04 +0300

    challenge one

---
+++ challange_one.py
@@ -0,0 +1,10 @@
+#Challange One
+"""Write a Python script that outputs ´"Hello, World!"´ using the print function.
+
+Requirements
+
+The program should output 'Hello, World!' exactly."""
+
+# Write your code here
+#printing Hello world
+print("Hello, World!")
