6538d3e86c20ae58f9eefdd93406b36b0174ac47
dagmawi77 <dagmawiletarik@gmail.com>
2025-03-11 07:15:53 +0300

    challenge 7 variable casting

---
+++ challange_7_varibale_cast.py
@@ -0,0 +1,23 @@
+"""Use casting to store the number 3 as a string, as an int, and as a float.
+
+Requirements
+
+x must be a string: "3"
+
+y must be an integer: 3
+
+z must be a float: 3.0"""
+
+
+# Use casting to make the values below the correct data type
+x = 3
+y = 3.7
+z = 3
+
+print(x)
+print(y)
+print(z)
+
+x = str(x)
+y = int(y)
+z = float(z)
