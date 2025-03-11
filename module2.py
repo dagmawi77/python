147b1cf0fec46dbc229ddf0f3db0fc444257ce83
dagmawi77 <dagmawiletarik@gmail.com>
2025-03-11 07:03:26 +0300

    module

---
+++ module2.py
@@ -0,0 +1,15 @@
+import module as m
+import platform
+m.greeting("dagmawi")
+x = platform.system()
+print(x)
+x=platform.architecture()
+print(x)
+x=platform.processor()
+print(x)
+x = dir(platform)
+print(x)
+
+a = m.person1["age"]
+print(a)
+
