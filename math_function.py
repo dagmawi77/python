4a10d46cea8eb0fd23cd5200ee9d3e2d9a400f9a
dagmawi77 <dagmawiletarik@gmail.com>
2025-03-12 06:41:34 +0300

    adding math function

---
+++ math_function.py
@@ -0,0 +1,25 @@
+import math
+
+x = min(5,20,10)
+y = max(5,20,10)
+
+print(x)
+print(y)
+
+x = abs(-8.45)
+print(x)
+
+y = pow(25,2)
+
+print(y)
+
+x = math.ceil(5.25)
+y = math.floor(5.25)
+print(x)
+print(y)
+
+x = math.pi
+print(x)
+
+x = math.sqrt(64)
+print(x)
