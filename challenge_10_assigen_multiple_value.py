18f7cda6cf912c1305b34f866b3d00e896a17d67
dagmawi77 <dagmawiletarik@gmail.com>
2025-03-12 07:13:44 +0300

    assigen Mutiple value

---
+++ challenge_10_assigen_multiple_value.py
@@ -0,0 +1,13 @@
+"""Assign values from a list to three separate variables.
+
+Requirements
+
+Variable x should be 'apple'.
+
+Variable y should be 'banana'.
+
+Variable z should be 'cherry'."""
+
+# Assign values from the list to three variables
+fruits = [ 'apple','banana','cherry']
+x, y, z = fruits
