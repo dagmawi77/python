0d0e46e5c13c28692f54926cf22058e7502848ed
dagmawi77 <dagmawiletarik@gmail.com>
2025-03-13 07:17:58 +0300

    module installation and using

--- module_installation.py
+++ module_installation.py
@@ -5,3 +5,22 @@ c = camelcase.CamelCase()
 txt = "hello world"
 
 print(c.hump(txt))
+
+"""
+Example
+Download a package named "camelcase":
+
+C:\Users\Your Name\AppData\Local\Programs\Python\Python36-32\Scripts>pip install camelcase
+"""
+
+"""Example
+Uninstall the package named "camelcase":
+
+C:\Users\Your Name\AppData\Local\Programs\Python\Python36-32\Scripts>pip uninstall camelcase"""
+
+#LIST Pacakge 
+
+"""Example
+List installed packages:
+
+C:\Users\Your Name\AppData\Local\Programs\Python\Python36-32\Scripts>pip list"""
