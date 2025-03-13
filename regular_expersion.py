70e2e28c7e4078c965865de7476274b8ee5dbaa2
dagmawi77 <dagmawiletarik@gmail.com>
2025-03-13 07:12:15 +0300

    Regular Expersion

---
+++ regular_expersion.py
@@ -0,0 +1,30 @@
+import re
+
+txt = "The rain in Spain"
+
+x= re.search("^The.*Spain$",txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.findall("ai", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.findall("Portugal", txt)
+print(x)
+txt = "The rain in Spain"
+x = re.search("\s", txt)
+
+print("The first white-space character is located in position:", x.start())
+
+txt = "The rain in Spain"
+x = re.search("Portugal", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.split("\s", txt)
+print(x)
+
+txt = "The rain in Spain"
+x = re.split("\s", txt, 1)
+print(x)
