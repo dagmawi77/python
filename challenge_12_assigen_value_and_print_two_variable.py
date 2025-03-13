5755ea2f3ff8ce5ceab686c91316b443fa0a37fb
dagmawi77 <dagmawiletarik@gmail.com>
2025-03-13 07:19:22 +0300

    print two variable

---
+++ challenge_12_assigen_value_and_print_two_variable.py
@@ -0,0 +1,17 @@
+"""Declare two variables, x and y. Then, print both variables in a single line.
+
+Requirements
+
+Variable x should be declared and assigned the integer 5.
+
+Variable y should be declared and assigned the string 'John'.
+
+The output should be '5 John' when x and y are printed."""
+# Declare variables
+
+# Print variables
+
+x = 5
+y = 'John'
+
+print(x,y)
