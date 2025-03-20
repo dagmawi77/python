340ee211db3fae561b0ee78510dbd005f627cf79
dagmawi77 <dagmawiletarik@gmail.com>
2025-03-20 06:36:28 +0300

    plot marking

--- marker_ploat.py
+++ marker_ploat.py
@@ -11,6 +11,7 @@ ypoint = num.array([3,8,1,10])
 pt.plot(ypoint,'o:g',ms=20,mec='r',mfc='b')
 pt.plot(ypoint, marker = 'o', ms = 20, mec = '#4CAF50', mfc = '#4CAF50')
 pt.plot(ypoint, marker = 'o', ms = 20, mec = 'hotpink', mfc = 'hotpink')
+pt.plot(ypoint,ls=':')
 
 
 pt.show()
