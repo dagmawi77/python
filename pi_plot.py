cc9fbf3e7fc88ec9745263d1c925f8f47d0b6198
Dagmawi Letarik <dagmawiletarik@gmail.com>
2025-03-22 21:16:10 +0300

    PI Plot

--- pi_plot.py
+++ pi_plot.py
@@ -12,3 +12,10 @@ mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
 
 plt.pie(y, labels = mylabels)
 plt.show() 
+
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+
+plt.pie(y, labels = mylabels, startangle = 90)
+plt.show() 
