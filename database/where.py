2250d750f1e4ad1a86b74c1cfdc818a64a1d9f08
dagmawi77 <dagmawiletarik@gmail.com>
2025-05-20 07:39:34 +0300

    whare codution

---
+++ database/where.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+mydb =mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="eschool"
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("SELECT * FROM customser where name ='Dagmawi'")
+myresult = mycursor.fetchall()
+for x in myresult:
+    print(x)
